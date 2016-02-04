684f6a5c83d75baf718969b190313d8e9b361994
531365872@qq.com <531365872@qq.com>
2016-02-05 03:46:53 +0800

    20160205

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
-/**
+/**************************************************************************
  * Created by PhpStorm.
  * User: Wolf
  * Date: 16-2-4
  * Time: 下午7:32
  * 入口文件
- */
+ * 作者：常炎隆
+ * Email：porschegt23@foxmail.com
+ **************************************************************************/
 
 define('rollerPHP' , dirname(__FILE__));
 define('System' ,'/System');
