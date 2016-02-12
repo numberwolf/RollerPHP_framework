704725ab715b21bd6cbd34655a56ed6dc82adbc4
531365872@qq.com <531365872@qq.com>
2016-02-12 16:48:20 +0800

    update

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 git add .;
 git commit -m "update";
 git push origin document;
-ssh -p root@123.56.154.87 'cd /var/www/html/RollerDoc && git pull origin document';  
+ssh -p 22 root@123.56.154.87 'cd /var/www/html/RollerDoc && git pull origin document';  
