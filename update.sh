10fa61e23f75fa419006a2bb942af7bfef097a32
numberwolf <porschegt23@foxmail.com>
2017-01-04 01:07:21 +0800

    update

--- update.sh
+++ update.sh
@@ -4,6 +4,6 @@ git add -A;
 read -p "此次上传:"  val
 ##echo $val
 git commit -m $val
-git push all --all -f
+git push all --all
 #git push origin master -f
 #ssh -p 22 root@123.56.154.87 'cd /var/www/html/RollerPHP_framework && git pull origin master';  
